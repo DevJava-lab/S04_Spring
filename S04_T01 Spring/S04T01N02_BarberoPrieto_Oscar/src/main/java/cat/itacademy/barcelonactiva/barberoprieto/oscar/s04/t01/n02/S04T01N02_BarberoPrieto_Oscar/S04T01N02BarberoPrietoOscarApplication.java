34670ec0be44f1a8bc29deebe66ed82155fca033
Òscar BarberoPrieto <118044020+DevJava-lab@users.noreply.github.com>
2023-06-24 20:47:50 +0200

    Exercici N02

---
+++ S04_T01 Spring/S04T01N02_BarberoPrieto_Oscar/src/main/java/cat/itacademy/barcelonactiva/barberoprieto/oscar/s04/t01/n02/S04T01N02_BarberoPrieto_Oscar/S04T01N02BarberoPrietoOscarApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.barberoprieto.oscar.s04.t01.n02.S04T01N02_BarberoPrieto_Oscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02BarberoPrietoOscarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02BarberoPrietoOscarApplication.class, args);
+	}
+
+}
