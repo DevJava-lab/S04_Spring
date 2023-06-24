79dc321e4149f82c11481b2493af41f2b74b2fd5
Òscar BarberoPrieto <118044020+DevJava-lab@users.noreply.github.com>
2023-06-24 20:12:10 +0200

    Creació repositori

---
+++ S04_T01 Spring/S04T01N01_BarberoPrieto_Oscar/src/test/java/cat/itacademy/barcelonactiva/barberoprieto/oscar/s04/t01/n01/S04T01N01_BarberoPrieto_Oscar/S04T01N01BarberoPrietoOscarApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.barberoprieto.oscar.s04.t01.n01.S04T01N01_BarberoPrieto_Oscar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01BarberoPrietoOscarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
